8252c6bd51ff602f62c2a7aaa4c5db07c4249443
Thomas Klebel <t.klebel@hotmail.com>
2022-04-24 10:29:59 +0200

    start constructing statistical model by simulating data

---
+++ bayes_model/01-simulate.R
@@ -0,0 +1,132 @@
+library(tidyverse)
+library(rethinking)
+
+set.seed(123)
+
+# simulate 1000 researchers
+n <- 1000
+
+# start by drawing randomly distributed ability
+df <- tibble(
+  r_ability = rnorm(n)
+)
+
+# sample available resources as weakly dependent on ability
+df <- df %>%
+  mutate(r_resources = rnorm(n, r_ability * .2))
+
+
+# compute and visualise interdependence
+m1  <- quap(
+  alist(
+    r_resources ~ dnorm(mu, sigma),
+    mu <- a + br_a*r_ability,
+    a ~ dnorm(0, 1),
+    br_a ~ dnorm(0, 1),
+    sigma ~ dexp(1)
+  ), data = df)
+precis(m1)
+
+
+df %>%
+  ggplot(aes(r_ability, r_resources)) +
+  geom_point() +
+  geom_abline(intercept = coef(m1)[1], slope = coef(m1)[2])
+
+
+# simulate paper quality
+df <- df %>%
+  mutate(paper_quality = rnorm(n, r_ability * .5 + r_resources * .2))
+
+# visualise again
+df %>%
+  ggplot(aes(paper_quality, r_ability)) +
+  geom_point()
+
+df %>%
+  ggplot(aes(paper_quality, r_resources)) +
+  geom_point()
+
+# compute biased effect of resources on paper quality (without accounting for
+# r_ability)
+m2.1  <- quap(
+  alist(
+    paper_quality ~ dnorm(mu, sigma),
+    mu <- a + br_r*r_resources,
+    a ~ dnorm(0, 1),
+    br_r ~ dnorm(0, 1),
+    sigma ~ dexp(1)
+  ), data = df)
+precis(m2.1)
+
+# now correct for ability
+m2.2  <- quap(
+  alist(
+    paper_quality ~ dnorm(mu, sigma),
+    mu <- a + br_r*r_resources + br_a*r_ability,
+    a ~ dnorm(0, 1),
+    br_r ~ dnorm(0, 1),
+    br_a ~ dnorm(0, 1),
+    sigma ~ dexp(1)
+  ), data = df)
+precis(m2.2)
+
+
+# disregard journal for now, sample oa status directly
+# for now model assume no influence of paper quality
+df <- df %>%
+  mutate(apc_oa = rbernoulli(n, 1/(1 + exp(0 - .5 * r_resources))))
+df
+# could also think about modelling more papers per researcher, thus using
+# a binomial model
+
+# model
+m3.1  <- quap(
+  alist(
+    apc_oa ~ dbinom(1, p),
+    logit(p) <- a + br_r*r_resources,
+    a ~ dnorm(0, 1),
+    br_r ~ dnorm(0, 1)
+    ), data = df)
+precis(m3.1)
+exp(.45)
+# -> check the above with posterior samples etc. to better understand effect
+
+# and include paper quality for now
+df <- df %>%
+  mutate(apc_oa = rbernoulli(n, 1/(1 + exp(0 - .5 * r_resources -
+                                             .5 * paper_quality))))
+df
+
+# model total causal effect
+m3.2  <- quap(
+  alist(
+    apc_oa ~ dbinom(1, p),
+    logit(p) <- a + br_r*r_resources,
+    a ~ dnorm(0, 1),
+    br_r ~ dnorm(0, 1)
+  ), data = df)
+precis(m3.2)
+exp(.64)
+# -> check the above with posterior samples etc. to better understand effect
+# total causal effect is more than just direct effect of resources on apc_oa,
+# since resources also have effect via paper quality
+
+# model direct effect by accounting for ability
+m3.3  <- quap(
+  alist(
+    apc_oa ~ dbinom(1, p),
+    logit(p) <- a + br_r*r_resources + br_a*r_ability,
+    a ~ dnorm(0, 1),
+    br_r ~ dnorm(0, 1),
+    br_a ~ dnorm(0, 1)
+  ), data = df)
+precis(m3.3)
+# now accounting for ability, we get the direct effect back
+
+# next steps:
+# - check the models' posteriors to get a better feel and understanding for how
+# to do this
+# - Include journals
+# - construct measurement error model for researcher ability
+# - move on to more complicated DAG
