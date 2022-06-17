fa19f399cc48a00540d6ed96d854e79c4ea62443
Thomas Klebel <t.klebel@hotmail.com>
2022-06-17 18:08:25 +0200

    simplify naming of parameters
    indicator variables are not necessary for brms

--- bayes_model/11-varying-slopes.R
+++ bayes_model/11-varying-slopes.R
@@ -1,52 +1,28 @@
 library(tidyverse)
 library(brms)
-library(brmstools)
+library(brmstools) # see how this can be replaced by tidybayes
+library(tidybayes)
 
 df <- read_csv("data/processed/multilevel_sample.csv")
 
 # make model df
 base <- df %>%
-  select(institution_id, country, author_position, PP_top10, display_name,
+  select(institution_id, country, author_position, PP_top10,
+         field = display_name,
          APC_in_dollar) %>%
   filter(!is.na(APC_in_dollar))
 
-
-mdf <- base %>%
-  # calculate indicator variables
-  mutate(across(c(institution_id, country, author_position, display_name),
-                ~as.factor(.x) %>% as.numeric(), .names = "{.col}_indicator"))
-
-countries <- mdf %>%
-  select(contains("country")) %>%
-  distinct() %>%
-  arrange(country_indicator)
-countries
-
-fields <- mdf %>%
-  select(contains("display")) %>%
-  distinct() %>%
-  arrange(display_name_indicator)
-
-dlist <- list(
-  country = mdf$country_indicator,
-  institute = mdf$institution_id_indicator,
-  field = mdf$display_name,
-  author_position = mdf$author_position_indicator,
-  inst_res = scale(mdf$PP_top10),
-  apc_price = scale(mdf$APC_in_dollar)
-)
-
 # start with model that incorporates all varying intercepts
-m1 <- brm(apc_price ~ 1 + inst_res + (1|country) + (1|institute) +
+m1 <- brm(APC_in_dollar ~ 1 + PP_top10 + (1|country) + (1|institution_id) +
             (1|author_position) + (1|field),
           family = "gaussian",
           prior = c(prior(normal(0, .5), class = Intercept),
                     prior(normal(0, .5), class = b),
                     prior(exponential(1), class = sd),
                     prior(exponential(1), class = sigma)),
-          data = dlist,
+          data = base,
           chains = 4, cores = 4,
-          control = list(adapt_delta = .85),
+          control = list(adapt_delta = .95),
           save_model = "bayes_model/m1.stan",
           file = "bayes_model/m1")
 summary(m1)
@@ -55,17 +31,17 @@ launch_shinystan(m1)
 
 
 # and now let intercepts vary by country and field
-m2 <- brm(apc_price ~ 1 + inst_res + (1 + inst_res|country) + (1|institute) +
-            (1|author_position) + (1 + inst_res|field),
+m2 <- brm(APC_in_dollar ~ 1 + PP_top10 + (1 + PP_top10|country) + (1|institution_id) +
+            (1|author_position) + (1 + PP_top10|field),
           family = "gaussian",
           prior = c(prior(normal(0, .5), class = Intercept),
                     prior(normal(0, .5), class = b),
                     prior(exponential(1), class = sd),
                     prior(exponential(1), class = sigma),
                     prior(lkj(2), class = cor)),
-          data = dlist,
-          warmup = 1000, iter = 2000, chains = 4, cores = 4,
-          control = list(adapt_delta = .95),
+          data = base,
+          warmup = 2000, iter = 3000, chains = 4, cores = 4,
+          control = list(adapt_delta = .98),
           save_model = "bayes_model/m2.stan",
           file = "bayes_model/m2")
 # 76 divergent transitions,
@@ -77,22 +53,22 @@ ranef(m2)
 # very interesting: negative correlation between intercept and pptop10 in
 # countries: so for countries paying high APCs, pptop has a negative effect
 # but positive correlation for fields: low APC, low effect, high APC, high
-# impact of inst_res
+# impact of PP_top10
 #
 # Overall much more variance between fields than countries.
 #
-# overall positive effect of inst_res. "negative" values within fields and
+# overall positive effect of PP_top10. "negative" values within fields and
 # countries need to be taken into context here
 #
 # todos:
 # - rerun with higher adapt_delta and longer warm-up
 # - visualise draws for fields and for countries -> there maybe link to regions
 # (income or continent)
-# - check with a naive model that models inst_res simply per country (no pooling)
+# - check with a naive model that models PP_top10 simply per country (no pooling)
 # -
 
 # look at visualising
-res <- as_draws_df(m2, variable = "inst_res", regex = TRUE)
+res <- as_draws_df(m2, variable = "PP_top10", regex = TRUE)
 
 forest(m2, "field") # very interesting
 ggsave("bayes_model/plots/random_effect_fields.png", width = 14, height = 6)
@@ -100,9 +76,11 @@ ggsave("bayes_model/plots/random_effect_fields.png", width = 14, height = 6)
 # population level effects
 forest(m2, "country")
 
+# here try to find an equivalent with tidybayes
+
 # general parameter
 res %>%
-  ggplot(aes(b_inst_res)) +
+  ggplot(aes(b_PP_top10)) +
   geom_density()
 
 conditional_fields <- fields %>%
