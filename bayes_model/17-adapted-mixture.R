27fabb90e77ede0111230dfe6fdc2781775c7eef
Thomas Klebel <t.klebel@hotmail.com>
2022-07-04 20:42:57 +0200

    prepare code for running adapted stanfile and integrate preprocessing code into markdown file

--- bayes_model/17-adapted-mixture.R
+++ bayes_model/17-adapted-mixture.R
@@ -2,9 +2,7 @@ library(tidyverse)
 library(brms)
 library(cmdstanr)
 
-options(mc.cores = 4,
-        brms.backend = "cmdstanr",
-        brms.file_refit = "on_change")
+options(mc.cores = 4)
 
 df <- read_csv("data/processed/multilevel_sample.csv")
 wdi <- WDI::WDI_data$country %>%
@@ -19,11 +17,6 @@ base <- df %>%
   mutate(APC_in_dollar = case_when(is.na(APC_in_dollar) ~ 0,
                                    TRUE ~ APC_in_dollar)) %>%
   mutate(P_top10 = scale(log(P_top10), scale = FALSE))
-
-subsample <- base %>%
-  slice_sample(n = 1000)
-
-
 
 # family
 mix <- mixture(hurdle_lognormal(), hurdle_lognormal(), order = TRUE)
@@ -57,46 +50,25 @@ priors_narrower <- c(
   prior(lkj(4), class = cor)
 )
 
-set.seed(2342)
+
 fitting_data <- make_standata(model_formula,
-                              data = slice_sample(base, n = 10000),
-                              family = mix, prior = priors_narrower,
-                              internal = TRUE)
-attributes(fitting_data)
+                              data = base,
+                              family = mix,
+                              prior = priors_narrower)
 
-# mod <- cmdstan_model(stan_file = "bayes_model/17-adapted-mixture.stan")
-#
-# fit <- mod$sample(
-#   data = fitting_data,
-#   seed = 123,
-#   chains = 4,
-#   parallel_chains = 4,
-#   iter_warmup = 500,
-#   iter_sampling = 500,
-#   refresh = 5,
-#   init = 0
-# )
-fit <- readRDS("bayes_model/final_models/17-try-1.rds")
+mod <- cmdstan_model(stan_file = "bayes_model/17-adapted-mixture.stan")
 
-summary_obj <- fit$summary()
+fit <- mod$sample(
+  data = fitting_data,
+  seed = 123,
+  chains = 4,
+  parallel_chains = 4,
+  iter_warmup = 1000,
+  iter_sampling = 1000,
+  refresh = 20,
+  init = 0
+)
 
+fit$save_object(file = "bayes_model/final_models/17-vienna-1.rds")
 
-fit$diagnostic_summary()
-
-stanfit <- rstan::read_stan_csv(fit$output_files())
-launch_shinystan(stanfit)
-
-fit$save_object(file = "bayes_model/final_models/17-try-1.rds")
-
-# workflow for getting back parameters after fitting adapted model
-# adapted from https://github.com/paul-buerkner/brms/issues/745
-
-mix_identified <- brm(model_formula,
-                      family = mix,
-                      prior = priors_narrower,
-                      data = base,
-                      empty = TRUE)
-mix_identified$fit <- stanfit
-mix_identified <- rename_pars(mix_identified)
-summary(mix_identified)
 
