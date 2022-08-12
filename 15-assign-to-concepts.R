a961cef182ca170aa2b01e9d22169721c9dd18cc
Thomas Klebel <t.klebel@hotmail.com>
2022-08-12 16:54:12 +0200

    we need an inner join - only retaining articles that actually have a top-level concept

--- 15-assign-to-concepts.R
+++ 15-assign-to-concepts.R
@@ -8,7 +8,7 @@ message("Connecting to spark...")
 
 config <- spark_config()
 config$spark.executor.cores <- 5 # this should always stay the same
-config$spark.executor.instances <- 20 # this can go up to 27, depending on RAM
+config$spark.executor.instances <- 27 # this can go up to 27, depending on RAM
 config$spark.executor.memory <- "12G"
 sc <- spark_connect(master = "yarn-client", config = config,
                     app_name = "assign_to_concepts")
@@ -38,10 +38,11 @@ works_with_concept_id <- works %>%
   left_join(works_concepts, by = c("id" = "work_id"))
 # sdf_register(works_with_concept_id, "works_with_concept_id")
 # tbl_cache(sc, "works_with_concept_id")
+# works_with_concept_id <- tbl(sc, "works_with_concept_id") # need to overwrite
+# # the R object, otherwise the whole caching part is not being used
 
 work_ids_with_concepts <- works_with_concept_id %>%
-  left_join(concepts, by = c("concept_id" = "id")) %>%
-  filter(!is.na(level))
+  inner_join(concepts, by = c("concept_id" = "id"))
 
 concept_fraction <- work_ids_with_concepts %>%
   group_by(id) %>%
