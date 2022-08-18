05523b245d96189e1c9a7e7b725dab744c820eec
Thomas Klebel <t.klebel@hotmail.com>
2022-08-18 13:36:46 +0200

    commit model code from a couple of days ago
    this code runs the basic version of the hurdle model, with only one mixture component (1 hurdle + 1 lognormal)

--- bayes_model/13-final-hurdle-model.R
+++ bayes_model/13-final-hurdle-model.R
@@ -5,14 +5,14 @@ options(mc.cores = 4,
         brms.backend = "cmdstanr",
         brms.file_refit = "on_change")
 
-df <- read_csv("data/processed/multilevel_sample.csv")
+df <- read_csv("data/processed/multilevel_sample_large.csv")
 wdi <- WDI::WDI_data$country %>%
   as_tibble() %>%
   select(iso2c, region, income)
 
 base <- df %>%
   left_join(wdi, by = c("country_code" = "iso2c")) %>%
-  select(institution_id, University, country, region, author_position,
+  select(id, institution_id, University, country, region, author_position,
          P_top10, field,
          APC_in_dollar, total_weight) %>%
   mutate(APC_in_dollar = case_when(is.na(APC_in_dollar) ~ 0,
@@ -22,17 +22,17 @@ base <- df %>%
 model_formula <- bf(
   APC_in_dollar | weights(total_weight) ~ 1 + P_top10 + (1 + P_top10|country) +
     (1 + P_top10|field),
-  hu ~ 1 + P_top10 + (1 + P_top10|country) +
-    (1 + P_top10|field)
+  hu ~ 1 + P_top10 + (1 + P_top10|country) + (1 + P_top10|field)
 )
 
 priors <- c(
   prior(normal(7.5, 2), class = Intercept),
+  prior(normal(0, 1), class = Intercept, dpar = hu),
   prior(normal(0, 1), class = b),
   prior(normal(0, 1), class = b, dpar = hu),
   prior(normal(0, 1), class = sd),
   prior(normal(0, 1), class = sigma),
-  prior(lkj(2), class = cor)
+  prior(lkj(4), class = cor)
 )
 
 hm_final <- brm(model_formula,
@@ -40,5 +40,5 @@ hm_final <- brm(model_formula,
                 prior = priors,
                 data = base,
                 seed = 1234,
-                file = "bayes_model/final_models/hm_final")
+                file = "bayes_model/final_models/hm_final_rerun")
 # 801 minutes (13.3 hours), but with short breaks of computation
